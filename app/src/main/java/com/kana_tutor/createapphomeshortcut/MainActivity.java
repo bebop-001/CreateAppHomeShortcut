822e916ff659bf8f0ada61e557a3a04ee0fab55f
sjs <sjs@sonic.net>
2018-10-20 15:06:18 -0700

    We now start MainActivity at startup and CreateShortcut is invoked with startIntentForResult.

---
+++ app/src/main/java/com/kana_tutor/createapphomeshortcut/MainActivity.java
@@ -0,0 +1,42 @@
+package com.kana_tutor.createapphomeshortcut;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+    private static String TAG = "MainActivity";
+
+    public static final int CREATE_SHORTCUT = 99;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+        findViewById(R.id.create_shortcut).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, CreateShortcut.class);
+                i.putExtra("shortcutId", getString(R.string.app_name));
+                startActivityForResult(i, CREATE_SHORTCUT);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (requestCode == CREATE_SHORTCUT) {
+            if(resultCode == Activity.RESULT_OK){
+                String result=data.getStringExtra("result");
+                Log.d(TAG, "CreateShortcut result:" + result);
+            }
+            else if (resultCode == Activity.RESULT_CANCELED) {
+                //Write your code if there's no result
+            }
+        }
+    }//onActivityResult
+}
